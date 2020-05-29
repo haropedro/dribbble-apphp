64efcc7867539dda8ac253cbfa641727595af86b
Harold Pedroso <haropedro@hotmail.com>
2020-05-29 07:31:22 -0700

    Adding the javascript to the shot interface and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
-  root 'home#index'
   resources :shots
+  devise_for :users, controllers: { registrations: 'registrations' }
+  root 'shots#index'
+
 
 end
