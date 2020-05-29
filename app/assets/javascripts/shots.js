e192250d408ffb35c51b0f0200bd140d513a1779
Harold Pedroso <haropedro@hotmail.com>
2020-05-29 13:02:24 -0700

    Making the app production ready and adding shots functionality

--- app/assets/javascripts/shots.js
+++ app/assets/javascripts/shots.js
@@ -103,3 +103,13 @@ document.addEventListener("turbolinks:load", function() {
 
 
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :shots
+
+  resources :shots do
+    resources :comments
+  end
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'shots#index'
 
