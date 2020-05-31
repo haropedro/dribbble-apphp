2b8b26988cecf651bd03cee92e567581de128e16
Harold Pedroso <haropedro@hotmail.com>
2020-05-30 17:13:09 -0700

    Last commit of the dribble app

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,6 +1,6 @@
 class ShotsController < ApplicationController
-  before_action :set_shot, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :set_shot, only: [:show, :edit, :update, :destroy, :like, :unlike]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :like, :unlike]
   #impressionist actions: [:show], unique: [:impressionable_type, :impressionable_id, :session_hash]
 
   # GET /shots
@@ -62,6 +62,26 @@ class ShotsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+
+  def like
+     @shot.liked_by current_user
+     respond_to do |format|
+       format.html { redirect_back fallback_location: root_path }
+       format.json { render layout:false }
+     end
+   end
+
+ def unlike
+   @shot.unliked_by current_user
+   respond_to do |format|
+     format.html { redirect_back fallback_location: root_path }
+     format.json { render layout:false }
+   end
+ end
+
+
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
